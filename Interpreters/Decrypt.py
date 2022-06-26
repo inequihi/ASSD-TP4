42d3f81ef7ff60071973a4ebf4ebb255d16fc203
Ine Qui <inequihillalt@gmail.com>
2022-06-26 15:03:31 -0300

    byte_to_fft listo

--- Interpreters/Decrypt.py
+++ Interpreters/Decrypt.py
@@ -1,12 +1,54 @@
 import Interpreter
+import numpy as np
 
 ##################################
 #           DECRYPT              #
 ##################################
 # Funciones
 #   FFT_ASCII_to_encrypt --> Parsea array FFT con contenido ASCII a string de bytes encriptado
-#  Byte_to_FFT --> Parsea el string the bytes desencriptado y lo acomoda en un FFT array
+#   Byte_to_FFT --> Parsea el string the bytes desencriptado y lo acomoda en un FFT array
 
 class Decrypt(Interpreter):
     def __init__(self):
         self.time_samples = None
+
+    def Byte_to_FFT(self, byte_fft):
+        i = 0
+        k = 0
+        size_FFT_arr = 0
+        string_fft = byte_fft.decode(encoding='utf-8')
+
+        # Obtengo size del FFT array
+        mas = string_fft.count('+')  # +1+2+4+2-33+4 --> +1+2j  +4+2k -33+4j   --> 6 signos --> 3 elementos de matriz
+        menos = string_fft.count('-')
+        if ((mas + menos) % 2):
+            print("ERROR EN TAMAÑO DE BYTE_FFT")
+        else:
+            size_FFT_arr = int((mas + menos) / 2)
+
+        # Creo array de FFTa
+        FFTa = np.zeros((size_FFT_arr, 2))
+        indexTemp = 0
+
+        for i in range(size_FFT_arr):
+
+            for j in range(2):
+                signoTemp = ""
+                StringTemp = ""
+                # Primer for loop para obtener un stirng de un numero
+                for k in range(indexTemp, len(string_fft) - 1):
+                    if ((string_fft[k] == '+') or (string_fft[k] == '-')):  # Interpreto el signo si lo encuentro
+                        signoTemp = string_fft[k]
+                        print("Signo temp:", signoTemp)
+
+                    if ((string_fft[k + 1] != '+') and (string_fft[k + 1] != '-')):  # Sumo 1 pq el indice k empieza en el signo
+                        StringTemp += string_fft[k + 1]
+                        print(StringTemp)
+                    else:
+                        indexTemp = k + 1
+                        break
+                FFTa[i][j] = float(StringTemp)  # Asigno valores
+                if (signoTemp == '-'):
+                    FFTa[i][j] = FFTa[i][j] * -1
+        return FFTa
+
